994203c72a9d10e3c4ca6a46ff8df7900d8aed9f
adrian-lys-dev <drugakadrian@gmail.com>
2025-09-13 21:25:21 +0300

    apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "",
+  apiUrl: "https://foxwithbook-server.azurewebsites.net/api/",
   clientId: "471111158934-oof4bcijqe0tv89g25ppvia4dlkddir9.apps.googleusercontent.com",
 };
